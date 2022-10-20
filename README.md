ad4eaa787670d5b96959a1e175cc276d4949a6a3
Marco Ostaska <marcoan@kyndryl.com>
2022-10-19 23:16:50 -0300

    first release

---
+++ README.md
@@ -0,0 +1,165 @@
+# Fair StandBy On-Call Scheduler
+
+Fair StandBy on-Call Scheduler is a python script that aims to create a fair on-call table using a set of rules:
+
+## Rules
+- Try to alternate as possible the days between the list
+- Try to avoid a person to work two weekend in a row
+- Try to avoid whoever worked on weekend to work on monday
+
+
+## How to Install
+
+In order to use this script, you will need to download and install [python 3](https://www.python.org/downloads/), alternately you can install [miniconda](https://docs.conda.io/en/latest/miniconda.html)
+
+#### Download and install the code
+
+```
+git clone https://github.com/marco-ostaska/fair_stdby_gen
+cd fair_stdby_gen
+```
+
+#### Install the required python modules
+
+```
+pip install -r requirements.txt
+```
+
+or
+```
+pip3 install -r requirements.txt
+```
+
+## How to configure
+
+Edit the file `config/config.ymal` *(DO NOT RENAME THIS FILE)* and follow comments instructions as the example below:
+ - A lot can be accomplished tunning this file.
+
+```yaml
+# Settings file in YAML
+# DO NOT RENAME THIS FILE
+
+# Year in YYYY format
+year: 2022
+
+# Month in mm format
+month: 11
+
+# List of holidays in this month
+# Enter an empty list in case of none
+Holidays:
+    - 2
+    - 15
+    - 20
+
+# List of people that will be on the standby schedule
+# Should be at leat two
+People:
+    - { Name: "Person 1",            # Person Name
+       idx: 0,                       # Person index, must be unique and sequential
+       week_restriction: [],         # Week restrictions (Days of the week a person can´t work). (0=Monday, 6=Sunday)
+       day_restriction: [],          # List of days a person cant work, useful for vacation
+       wanted_days: [] }             # A list of days a person wants to work
+
+
+    - { Name: "Person 2",
+       idx: 1,
+       week_restriction: [],
+       day_restriction: [],
+       wanted_days: [] }
+
+    - { Name: "Person 3",
+       idx: 2,
+       week_restriction: [],
+       day_restriction: [],
+       wanted_days: [] }
+
+
+    - { Name: "Person 4",
+       idx: 3,
+       week_restriction: [2, 3],
+       day_restriction: [],
+       wanted_days: [] }
+
+    - { Name: "Person 5",
+       idx: 4,
+       week_restriction: [],
+       day_restriction: [15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30],
+       wanted_days: [] }
+
+
+# If someone worked last Saturday use this parameter with the person name
+last_saturday: "Person 1"
+# If someone worked last Saturday use this parameter with the person name
+last_sunday: "Person 4"
+```
+
+## How to use
+
+```
+python fair_stdby_gen.py
+```
+
+# The Output
+
+### Summary
+
+- **Hours:** the sum of the hours a person would be on-call, where regular weekdays = 15 hors. Weekends and holidays = 24 hours
+- **Days:** number of days a person would be on call
+- **15h day:** number of regular week days a person would be on call
+- **24h day:** number of 24h week days (weekends and holidays) a person would be on call
+
+```
++------------+---------+--------+-----------+-----------+
+| Person 1   |   Hours |   Days |   15h day |   24h day |
+|------------+---------+--------+-----------+-----------|
+|            |     132 |      7 |         4 |         3 |
++------------+---------+--------+-----------+-----------+
+
++------------+---------+--------+-----------+-----------+
+| Person 2   |   Hours |   Days |   15h day |   24h day |
+|------------+---------+--------+-----------+-----------|
+|            |     147 |      8 |         5 |         3 |
++------------+---------+--------+-----------+-----------+
+
++------------+---------+--------+-----------+-----------+
+| Person 3   |   Hours |   Days |   15h day |   24h day |
+|------------+---------+--------+-----------+-----------|
+|            |     123 |      7 |         5 |         2 |
++------------+---------+--------+-----------+-----------+
+
++------------+---------+--------+-----------+-----------+
+| Person 4   |   Hours |   Days |   15h day |   24h day |
+|------------+---------+--------+-----------+-----------|
+|            |      84 |      5 |         4 |         1 |
++------------+---------+--------+-----------+-----------+
+
++------------+---------+--------+-----------+-----------+
+| Person 5   |   Hours |   Days |   15h day |   24h day |
+|------------+---------+--------+-----------+-----------|
+|            |      54 |      3 |         2 |         1 |
++------------+---------+--------+-----------+-----------+
+
+```
+
+### The schedule
+
+```
++--------------+------------------------+--------------+------------------------+-----------------------+--------------+--------------+
+| Saturday     | Sunday                 | Monday       | Tuesday                | Wednesday             | Thursday     | Friday       |
+|--------------+------------------------+--------------+------------------------+-----------------------+--------------+--------------|
+|              |                        |              | 1. Person 1            | 2. Person 2 (holiday) | 3. Person 3  | 4. Person 4  |
+| 5. Person 5  | 6. Person 1            | 7. Person 2  | 8. Person 3            | 9. Person 4           | 10. Person 5 | 11. Person 1 |
+| 12. Person 2 | 13. Person 3           | 14. Person 5 | 15. Person 1 (holiday) | 16. Person 2          | 17. Person 3 | 18. Person 4 |
+| 19. Person 1 | 20. Person 4 (holiday) | 21. Person 2 | 22. Person 3           | 23. Person 4          | 24. Person 1 | 25. Person 2 |
+| 26. Person 3 | 27. Person 2           | 28. Person 1 | 29. Person 2           | 30. Person 3          |              |              |
++--------------+------------------------+--------------+------------------------+-----------------------+--------------+--------------+
+
+```
+
+## Limitations
+
+- Need to have at least 3 people on the list.
+- Depending on how many restrictions configured you may fall in a infinite loop, because the conditions could not be met. In case this happens hit `ctrl + c` to kill the script.
+
+
---
+++ fair_stdby_gen.py
@@ -0,0 +1,237 @@
+from operator import ge
+from random import random
+import yaml
+import calendar
+import random
+import numpy as np
+from tabulate import tabulate
+from yaml.loader import SafeLoader
+
+
+class Agenda:
+    Sat = 0
+    Sun = 1
+    Mon = 2
+    Tue = 3
+    Wed = 4
+    Thu = 5
+    Fri = 6
+
+    def __init__(self, data):
+        self.year = data["year"]
+        self.month = data["month"]
+        self.holidays = data["Holidays"]
+        calendar.setfirstweekday(calendar.SATURDAY)
+        self.month_calendar = calendar.monthcalendar(self.year, self.month)
+        self.month_schedule = [[""]*7 for i in range(len(self.month_calendar))]
+        self.init_saturday = data["last_saturday"]
+        self.init_sunday = data["last_sunday"]
+
+    def fill_month_schedule_wanted_days(self, people_list):
+        for week in range(len(self.month_calendar)):
+            for day in range(len(self.month_calendar[week])):
+                for person in people_list:
+                    if person.has_to_work(self.month_calendar[week][day]):
+                        self.month_schedule[week][day] = person.name
+                        break
+
+    def saturday_block(self, person, week) -> bool:
+        # is it the first saturday of month and person match the existing init saturday:
+        if (self.month_calendar[week][self.Sat] <= 7) and (self.init_saturday == person.name):
+            return True
+
+        # Person cant work on saturday if already worked on last one
+        if (self.month_schedule[week-1][self.Sat] == person.name):
+            return True
+
+        # Person has worked on last Friday
+        if (self.month_schedule[week-1][self.Fri] == person.name):
+            return True
+
+        # Person has worked on last Sunday
+        if person.total > 3 and (self.month_schedule[week-1][self.Sun] == person.name):
+            return True
+
+        return False
+
+    def sunday_block(self, person, week) -> bool:
+        # is it the first saturday of month and person match the existing init saturday:
+        if (self.month_calendar[week][self.Sun] <= 7) and (self.init_sunday == person.name):
+            return True
+
+        # Person cant work on sunday if already worked on last one
+        if (self.month_schedule[week-1][self.Sun] == person.name):
+            return True
+
+        # Person has worked on Saturday
+        if (self.month_schedule[week][self.Sat] == person.name):
+            return True
+
+        if person.total > 3:
+            # Person has worked on last Saturday
+            if self.month_schedule[week-1][self.Sat] == person.name:
+                return True
+
+        return False
+
+    def monday_block(self, person, week) -> bool:
+        # Has worked on Sunday
+        if (self.month_schedule[week][self.Sun] == person.name):
+            return True
+
+        # Has worked on Saturday
+        if (self.month_schedule[week][self.Sat] == person.name) and person.total > 2:
+            return True
+
+
+class Person:
+
+    def __init__(self, idx, name, week_restriction, day_restriction, wanted_days, total):
+        self.name = name
+        self.week_restriction = week_restriction
+        self.day_restriction = day_restriction
+        self.wanted_days = wanted_days
+        self.idx = idx
+        self.total = total
+
+    def has_week_restriction(self, week_day) -> bool:
+        if week_day in self.week_restriction:
+            return True
+
+    def has_day_restriction(self, month_day) -> bool:
+        if month_day in self.day_restriction:
+            return True
+
+    def has_to_work(self, month_day) -> bool:
+        if month_day in self.wanted_days:
+            return True
+
+    def available(self, month_day, week_day) -> bool:
+        if self.has_day_restriction(month_day):
+            return False
+
+        if self.has_week_restriction(week_day):
+            return False
+        return True
+
+
+def read_yaml(file_name):
+    with open(file_name, 'r') as stream:
+        try:
+            return yaml.load(stream, Loader=SafeLoader)
+        except yaml.YAMLError as exc:
+            print(exc)
+
+# Function to initialize objects
+
+
+def init_objects(data):
+    people_list = []
+    for people in data["People"]:
+        people_list.append(Person(people["idx"], people["Name"], people["week_restriction"],
+                                  people["day_restriction"], people["wanted_days"], len(data["People"])))
+
+    return people_list, Agenda(data)
+
+# function to get the next person from person list based on the last one used
+
+
+def get_next_person(people_list, current_person_idx, month_day, week_day) -> int:
+    for person in people_list:
+        if person.idx > current_person_idx:
+            if not person.available(month_day, week_day):
+                return get_next_person(people_list, person.idx,
+                                       month_day, week_day)
+
+            return person.idx
+    return people_list[0].idx
+
+
+def fill_schedule(agenda, person):
+
+    # Already fill the days a person need to work
+    agenda.fill_month_schedule_wanted_days(person)
+    person_idx = 0
+
+    for week in range(len(agenda.month_calendar)):
+        for day in range(len(agenda.month_calendar[week])):
+            if agenda.month_calendar[week][day] > 0 and agenda.month_schedule[week][day] == "":
+                # saturday
+                if day == agenda.Sat and agenda.month_schedule[week][day] == "":
+                    while agenda.saturday_block(person[person_idx], week):
+                        person_idx = get_next_person(
+                            person, person_idx, agenda.month_calendar[week][day], day)
+                    agenda.month_schedule[week][day] = person[person_idx].name
+                # Sunday
+                elif day == agenda.Sun and agenda.month_schedule[week][day] == "":
+                    while agenda.sunday_block(person[person_idx], week):
+                        person_idx = get_next_person(
+                            person, person_idx, agenda.month_calendar[week][day], day)
+                    agenda.month_schedule[week][day] = person[person_idx].name
+                # Monday
+                elif day == agenda.Mon and agenda.month_schedule[week][day] == "":
+                    while agenda.monday_block(person[person_idx], week):
+                        person_idx = get_next_person(
+                            person, person_idx, agenda.month_calendar[week][day], day)
+                    agenda.month_schedule[week][day] = person[person_idx].name
+                # other_days
+                elif day in range(3, 7) and agenda.month_schedule[week][day] == "":
+                    while agenda.month_schedule[week][day-1] == person[person_idx].name:
+                        person_idx = get_next_person(
+                            person, person_idx, agenda.month_calendar[week][day], day)
+                    agenda.month_schedule[week][day] = person[person_idx].name
+
+
+def print_summary(person, agenda):
+    for p in person:
+        sum_hours = 0
+        work_days = 0
+        day_24 = 0
+        day_15 = 0
+
+        for week in range(len(agenda.month_calendar)):
+            for day in range(0, 7):
+                if agenda.month_calendar[week][day] != 0 and agenda.month_schedule[week][day] == p.name:
+                    work_days += 1
+                    if agenda.month_calendar[week][day] in agenda.holidays or day == 0 or day == 1:
+                        sum_hours += 24
+                        day_24 += 1
+                    else:
+                        sum_hours += 15
+                        day_15 += 1
+
+        print(tabulate([['', sum_hours, work_days, day_15, day_24]],
+                       headers=[p.name, "Hours", 'Days', '15h day', '24h day'], tablefmt='psql'))
+        print()
+
+
+def print_schedule(agenda):
+    for week in range(len(agenda.month_calendar)):
+        for day in range(0, 7):
+            if agenda.month_calendar[week][day] != 0:
+                agenda.month_schedule[week][day] = "{}. {}".format(
+                    agenda.month_calendar[week][day], agenda.month_schedule[week][day])
+            if agenda.month_calendar[week][day] in agenda.holidays:
+                agenda.month_schedule[week][day] = "{} (holiday)".format(
+                    agenda.month_schedule[week][day])
+                print()
+    print(tabulate(agenda.month_schedule, headers=['Saturday', 'Sunday',
+                                                   'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'], tablefmt='psql'))
+
+
+def main():
+    data = read_yaml("./config/config.yaml")
+    person, agenda = init_objects(data)
+
+    # exit program if person.total < 3
+    if person[0].total < 3:
+        print("Not enough people to work")
+        exit(1)
+
+    fill_schedule(agenda, person)
+    print_summary(person, agenda)
+    print_schedule(agenda)
+
+
+if __name__ == "__main__":
+    main()
